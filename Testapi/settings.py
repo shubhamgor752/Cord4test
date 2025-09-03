4fd74b4054b6f3efa5811dd4bb62f8efd2995958
Shubhamgor752 <shubhamgor752@gmail.com>
2025-09-03 11:07:03 +0530

    added static file in setting

--- Testapi/settings.py
+++ Testapi/settings.py
@@ -144,6 +144,8 @@ AUTH_USER_MODEL = "Register.CustomUser"
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
